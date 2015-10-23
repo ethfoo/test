2c0eb3bd09ac0eee0f2b3f08e247f630222fd99f
ethfoo <ethfoo@gmail.com>
2015-10-23 12:49:03 +0800

    add code

--- test.c
+++ test.c
@@ -1,2 +1,17 @@
-test
-new_branch
+#include <stdio.h>
+
+int main(void)
+{
+
+	//int a,b,c;
+	//printf("输入a,b,c\n");
+	//scanf("%d%d%d",&a,&b,&c);
+	//printf("a=%d,b=%d,c=%d\n",a,b,c);
+	//fflush(stdin);
+
+	int a;
+	scanf("%d", &a);
+	printf("a =%d\n", a);
+
+	return 0;
+}
